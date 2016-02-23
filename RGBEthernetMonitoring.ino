0cefa6e1c6ec24e384b0e44bbaabfa27b0c6ca80
Corey Wood <sites@corky.me.uk>
2016-02-23 10:20:20 +0000

    Added brightness control sequence and better comments

--- RGBEthernetMonitoring.ino
+++ RGBEthernetMonitoring.ino
@@ -8,24 +8,23 @@
 
  created 22 Feb 2016
  by Corey Wood
- modified 22 Feb 2016
+ modified 23 Feb 2016
  by Corey Wood
 
  */
  
-// Initialising Pin outputs 
+//initialising pin outputs 
 int BLUE = 6;                   
 int GREEN = 5; 
 int RED = 3;
 int repeats = 0;
+int brightness = 0;
 String readString;
 
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network.
-// gateway and subnet are optional:
+//MAC address is set. values for ip, dns, gateway and subnet are added below
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
@@ -33,26 +32,27 @@ IPAddress ip(192, 168, 1, 177);
 IPAddress myDns(192,168,1, 1);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
-
-// Initialise the Ethernet server library
-// with the IP address and port you want to use
+//port for webserver
 EthernetServer server(80);
 
 void setup() {
-  // Open serial communications and wait for port to open:
+  //open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ; //wait for serial port to connect. needed for native USB port only
   }
-  // start the Ethernet connection and the server:
+  //start the ethernet connection and the server:
   Ethernet.begin(mac, ip);
   server.begin();
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
+  analogWrite(RED, 250);
+  analogWrite(BLUE, 166);
+  analogWrite(GREEN, 26);
 }
 
 void loop() {
-  // Create a client connection
+  //create a client connection
   EthernetClient client = server.available();
   if (client) {
     while (client.connected()) {   
@@ -62,6 +62,7 @@ void loop() {
         if (readString.length() < 100) {
           //store characters to string
           readString += c;
+          //debugging output
           //Serial.print(c);
          }
          //if HTTP request has ended
@@ -75,16 +76,23 @@ void loop() {
            client.println("<title>RGB LEDs</title>");
            client.println("</head>");
            client.println("<body>");
-           client.println("<br />");  
-           client.println("<a href=\"/?sequence1\"\">Sequence 1</a>"); //url addition
+           client.println("<br />");
+           //http request to trigger event
+           client.println("<a href=\"/?sequence1\"\">Sequence 1</a>");
            client.println("<br />");
            client.println("<a href=\"/?sequence2\"\">Sequence 2</a>");
            client.println("<br />");
+           client.println("<a href=\"/?sequence3\"\">Sequence 3</a>");
+           client.println("<br />");
+           client.println("<a href=\"/?sequence4\"\">Sequence 4</a>");
+           client.println("<br />");
            client.println("<a href=\"/?redon\"\">Turn On RED LED</a>");
-           client.println("<br />"); 
+           client.println("<br />");
            client.println("<a href=\"/?greenon\"\">Turn On GREEN LED</a>");
-           client.println("<br />"); 
+           client.println("<br />");
            client.println("<a href=\"/?blueon\"\">Turn On BLUE LED</a>");
+           client.println("<br />");
+           client.println("<a href=\"/?default\"\">Turn On Default Output</a>");
            client.println("<br />"); 
            client.println("</body>");
            client.println("</html>");
@@ -122,6 +130,37 @@ void loop() {
                analogWrite(BLUE, 166);
                analogWrite(GREEN, 26);
            }
+           if (readString.indexOf("?sequence3") >0){
+               for (repeats = 60; repeats > 0; repeats --) {
+                analogWrite(GREEN, 255);
+                analogWrite(RED, 0);
+                analogWrite(BLUE, 0);
+                delay(1000);
+               }
+               analogWrite(RED, 250);
+               analogWrite(BLUE, 166);
+               analogWrite(GREEN, 26);
+           }
+           if (readString.indexOf("?sequence4") >0){
+           //a fading LED
+               for (repeats = 15; repeats > 0; repeats --) {
+                  for (brightness = 0; brightness < 255; brightness ++) {
+                      analogWrite(GREEN, brightness);
+                      analogWrite(RED, brightness);
+                      analogWrite(BLUE, brightness);
+                      delay(5);
+                  }
+                  for (brightness = 255; brightness > 0; brightness --) {
+                      analogWrite(GREEN, brightness);
+                      analogWrite(RED, brightness);
+                      analogWrite(BLUE, brightness);
+                      delay(5);
+                  }
+                  analogWrite(RED, 250);
+                  analogWrite(BLUE, 166);
+                  analogWrite(GREEN, 26);
+              }
+           }
            if (readString.indexOf("?redon") >0){
                analogWrite(RED, 255);
                analogWrite(GREEN, 0);
@@ -136,6 +175,11 @@ void loop() {
                analogWrite(RED, 0);
                analogWrite(GREEN, 0);
                analogWrite(BLUE, 255);
+           }
+           if (readString.indexOf("?default") >0){
+               analogWrite(RED, 250);
+               analogWrite(GREEN, 166);
+               analogWrite(BLUE, 26);
            }
            //clearing string for next read
            readString="";
