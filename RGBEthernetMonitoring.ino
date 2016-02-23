e74187c833d0027839ebf67ffd8ca1de2a6ec6e5
Corey Wood <sites@corky.me.uk>
2016-02-23 12:26:24 +0000

    Indentation Fixes

--- RGBEthernetMonitoring.ino
+++ RGBEthernetMonitoring.ino
@@ -55,7 +55,7 @@ void loop() {
   //create a client connection
   EthernetClient client = server.available();
   if (client) {
-    while (client.connected()) {   
+    while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         //read char by char HTTP request
@@ -67,122 +67,122 @@ void loop() {
          }
          //if HTTP request has ended
          if (c == '\n') {          
-           Serial.println(readString); //print to serial monitor for debugging
-           client.println("HTTP/1.1 200 OK"); //send new page
-           client.println("Content-Type: text/html");
-           client.println();     
-           client.println("<html>");
-           client.println("<head>");
-           client.println("<title>RGB LEDs</title>");
-           client.println("</head>");
-           client.println("<body>");
-           client.println("<br />");
-           //http request to trigger event
-           client.println("<a href=\"/?sequence1\"\">Sequence 1</a>");
-           client.println("<br />");
-           client.println("<a href=\"/?sequence2\"\">Sequence 2</a>");
-           client.println("<br />");
-           client.println("<a href=\"/?sequence3\"\">Sequence 3</a>");
-           client.println("<br />");
-           client.println("<a href=\"/?sequence4\"\">Sequence 4</a>");
-           client.println("<br />");
-           client.println("<a href=\"/?redon\"\">Turn On RED LED</a>");
-           client.println("<br />");
-           client.println("<a href=\"/?greenon\"\">Turn On GREEN LED</a>");
-           client.println("<br />");
-           client.println("<a href=\"/?blueon\"\">Turn On BLUE LED</a>");
-           client.println("<br />");
-           client.println("<a href=\"/?default\"\">Turn On Default Output</a>");
-           client.println("<br />"); 
-           client.println("</body>");
-           client.println("</html>");
-           delay(1);
-           //stopping client
-           client.stop();
-           //these commands read the appended url addition and activate the LEDs accordingly
-           if (readString.indexOf("?sequence1") >0){
-               for (repeats = 10; repeats > 0; repeats --) {
-                analogWrite(GREEN, 0);
-                analogWrite(RED, 255);
-                analogWrite(BLUE, 0);
-                delay(500);
-                analogWrite(GREEN, 0);
-                analogWrite(RED, 0);
-                analogWrite(BLUE, 0);
-                delay(500);
-               }
-               analogWrite(RED, 250);
-               analogWrite(BLUE, 166);
-               analogWrite(GREEN, 26);
-           }
-           if (readString.indexOf("?sequence2") >0){
-               for (repeats = 5; repeats > 0; repeats --) {
-                analogWrite(GREEN, 0);
-                analogWrite(RED, 0);
-                analogWrite(BLUE, 255);
-                delay(500);
-                analogWrite(GREEN, 0);
-                analogWrite(RED, 0);
-                analogWrite(BLUE, 0);
-                delay(500);
-               }
-               analogWrite(RED, 250);
-               analogWrite(BLUE, 166);
-               analogWrite(GREEN, 26);
-           }
-           if (readString.indexOf("?sequence3") >0){
-               for (repeats = 60; repeats > 0; repeats --) {
-                analogWrite(GREEN, 255);
-                analogWrite(RED, 0);
-                analogWrite(BLUE, 0);
-                delay(1000);
-               }
-               analogWrite(RED, 250);
-               analogWrite(BLUE, 166);
-               analogWrite(GREEN, 26);
-           }
-           if (readString.indexOf("?sequence4") >0){
-           //a fading LED
-               for (repeats = 15; repeats > 0; repeats --) {
-                  for (brightness = 0; brightness < 255; brightness ++) {
-                      analogWrite(GREEN, brightness);
-                      analogWrite(RED, brightness);
-                      analogWrite(BLUE, brightness);
-                      delay(5);
-                  }
-                  for (brightness = 255; brightness > 0; brightness --) {
-                      analogWrite(GREEN, brightness);
-                      analogWrite(RED, brightness);
-                      analogWrite(BLUE, brightness);
-                      delay(5);
-                  }
-                  analogWrite(RED, 250);
-                  analogWrite(BLUE, 166);
-                  analogWrite(GREEN, 26);
+          Serial.println(readString); //print to serial monitor for debugging
+          client.println("HTTP/1.1 200 OK"); //send new page
+          client.println("Content-Type: text/html");
+          client.println();     
+          client.println("<html>");
+          client.println("<head>");
+          client.println("<title>RGB LEDs</title>");
+          client.println("</head>");
+          client.println("<body>");
+          client.println("<br />");
+          //http request to trigger event
+          client.println("<a href=\"/?sequence1\"\">Sequence 1</a>");
+          client.println("<br />");
+          client.println("<a href=\"/?sequence2\"\">Sequence 2</a>");
+          client.println("<br />");
+          client.println("<a href=\"/?sequence3\"\">Sequence 3</a>");
+          client.println("<br />");
+          client.println("<a href=\"/?sequence4\"\">Sequence 4</a>");
+          client.println("<br />");
+          client.println("<a href=\"/?redon\"\">Turn On RED LED</a>");
+          client.println("<br />");
+          client.println("<a href=\"/?greenon\"\">Turn On GREEN LED</a>");
+          client.println("<br />");
+          client.println("<a href=\"/?blueon\"\">Turn On BLUE LED</a>");
+          client.println("<br />");
+          client.println("<a href=\"/?default\"\">Turn On Default Output</a>");
+          client.println("<br />"); 
+          client.println("</body>");
+          client.println("</html>");
+          delay(1);
+          //stopping client
+          client.stop();
+          //these commands read the appended url addition and activate the LEDs accordingly
+          if (readString.indexOf("?sequence1") >0){
+            for (repeats = 10; repeats > 0; repeats --) {
+              analogWrite(GREEN, 0);
+              analogWrite(RED, 255);
+              analogWrite(BLUE, 0);
+              delay(500);
+              analogWrite(GREEN, 0);
+              analogWrite(RED, 0);
+              analogWrite(BLUE, 0);
+              delay(500);
+            }
+            analogWrite(RED, 250);
+            analogWrite(BLUE, 166);
+            analogWrite(GREEN, 26);
+          }
+          if (readString.indexOf("?sequence2") >0){
+            for (repeats = 5; repeats > 0; repeats --) {
+              analogWrite(GREEN, 0);
+              analogWrite(RED, 0);
+              analogWrite(BLUE, 255);
+              delay(500);
+              analogWrite(GREEN, 0);
+              analogWrite(RED, 0);
+              analogWrite(BLUE, 0);
+              delay(500);
+            }
+            analogWrite(RED, 250);
+            analogWrite(BLUE, 166);
+            analogWrite(GREEN, 26);
+          }
+          if (readString.indexOf("?sequence3") >0){
+            for (repeats = 60; repeats > 0; repeats --) {
+              analogWrite(GREEN, 255);
+              analogWrite(RED, 0);
+              analogWrite(BLUE, 0);
+              delay(1000);
+            }
+            analogWrite(RED, 250);
+            analogWrite(BLUE, 166);
+            analogWrite(GREEN, 26);
+          }
+          if (readString.indexOf("?sequence4") >0){
+          //a fading LED
+            for (repeats = 15; repeats > 0; repeats --) {
+              for (brightness = 0; brightness < 255; brightness ++) {
+                analogWrite(GREEN, brightness);
+                analogWrite(RED, brightness);
+                analogWrite(BLUE, brightness);
+                delay(5);
               }
-           }
-           if (readString.indexOf("?redon") >0){
-               analogWrite(RED, 255);
-               analogWrite(GREEN, 0);
-               analogWrite(BLUE, 0);
-           }         
-           if (readString.indexOf("?greenon") >0){
-               analogWrite(RED, 0);
-               analogWrite(GREEN, 255);
-               analogWrite(BLUE, 0);
-           }
-           if (readString.indexOf("?blueon") >0){
-               analogWrite(RED, 0);
-               analogWrite(GREEN, 0);
-               analogWrite(BLUE, 255);
-           }
-           if (readString.indexOf("?default") >0){
-               analogWrite(RED, 250);
-               analogWrite(GREEN, 166);
-               analogWrite(BLUE, 26);
-           }
-           //clearing string for next read
-           readString="";
+              for (brightness = 255; brightness > 0; brightness --) {
+                analogWrite(GREEN, brightness);
+                analogWrite(RED, brightness);
+                analogWrite(BLUE, brightness);
+                delay(5);
+              }
+            }
+            analogWrite(RED, 250);
+            analogWrite(BLUE, 166);
+            analogWrite(GREEN, 26);
+          }
+          if (readString.indexOf("?redon") >0){
+            analogWrite(RED, 255);
+            analogWrite(GREEN, 0);
+            analogWrite(BLUE, 0);
+          }         
+          if (readString.indexOf("?greenon") >0){
+            analogWrite(RED, 0);
+            analogWrite(GREEN, 255);
+            analogWrite(BLUE, 0);
+          }
+          if (readString.indexOf("?blueon") >0){
+            analogWrite(RED, 0);
+            analogWrite(GREEN, 0);
+            analogWrite(BLUE, 255);
+          }
+          if (readString.indexOf("?default") >0){
+            analogWrite(RED, 250);
+            analogWrite(GREEN, 166);
+            analogWrite(BLUE, 26);
+          }
+          //clearing string for next read
+          readString="";
          }
       }
     }
