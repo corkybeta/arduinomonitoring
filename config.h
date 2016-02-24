02e39c0ce03c0bcb0f4bf6e6b6c3d4bb55144626
Corey Wood <sites@corky.me.uk>
2016-02-24 13:45:37 +0000

    Update config.h

--- config.h
+++ config.h
@@ -3,6 +3,15 @@ int RED = 3;
 int GREEN = 5;
 int BLUE = 6;
 
+//variables
+int repeats = 0;
+int brightness = 0;
+String readString;
+String standardMessage = "VPW Systems";
+int standardRed = 250;
+int standardGreen = 166;
+int standardBlue = 26;
+
 //MAC address is set. values for ip, dns, gateway and subnet are added below
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
@@ -13,15 +22,6 @@ IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 //port for webserver
 EthernetServer server(80);
-
-//variables
-int repeats = 0;
-int brightness = 0;
-String readString;
-String standardMessage = "VPW Systems";
-int standardRed = 250;
-int standardGreen = 166;
-int standardBlue = 26;
 
 //i2c LCD
 #define I2C_ADDR 0x27 //address needs to be set, differs between i2c controllers
